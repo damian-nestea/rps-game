6f12dcdd8930bd21b00644bf11a72a0e640e94a5
damian-nestea <damian@hotmail.cz>
2024-01-19 15:25:15 -0300

    refactor adding event to the player options to select an option

--- user-interface.js
+++ user-interface.js
@@ -2,18 +2,20 @@
 const buttonsPlayer = document.querySelectorAll("#playerSelection button");
 const startButton = document.querySelector("#start");
 const gamePlaySection = document.querySelectorAll(".game");
-let p1Selection = "";
+
+function removeChoose() {
+  buttonsPlayer.forEach((button) => button.classList.remove("choosen"));
+}
 
 //events
 buttonsPlayer.forEach((button) => {
   button.addEventListener("click", (e) => {
-    p1Selection = e.target.className;
-    console.log(p1Selection);
+    removeChoose();
+    e.target.classList.add("choosen");
   });
 });
 
 startButton.addEventListener("click", () => {
-  console.log("entrou aqui");
   startButton.classList.add("disabled");
   gamePlaySection.forEach((item) => {
     item.classList.remove("hide");
